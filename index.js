f75a3f1d3e9a2d16dec0f328355e5078088e88de
i-prince <molatonny@gmail.com>
2023-07-10 18:04:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+// Assignment part 1
+let num1 = 10;
+let num2 = 6.2;
+
+let multiply = num1 * num2;
+
+console.log (multiply);
+
+// Assignment part 2
+let random = (Math.ceil(Math.random() + 1));
+
+console.log(random);
+
+// Assignment part 3
+let num3 = 24;
+let num4 = 5;
+
+let mod = (num3 % num4);
+
+console.log(mod)
+
+// Assignment part 4
+let max = Math.max(5,10,15,20)
+
+console.log(max)
